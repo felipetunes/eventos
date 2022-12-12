6bebafa84ca64da8caa7f8e82723314a7c53d729
felipe.oliveira <felipe.oliveira@madis.com.br>
2022-12-12 16:33:46 -0300

    validação de campos para novo evento

--- src/View/evento-cadastro/index.js
+++ src/View/evento-cadastro/index.js
@@ -1,11 +1,12 @@
 import React, {useState, useEffect} from "react";
-import {useParams} from 'react-router-dom';
+import {Link, useParams} from 'react-router-dom';
 import {db, storage} from '../../config/firebase';
-import {useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import Navbar from "../../components/navbar";
-import {collection, doc, addDoc, getDoc, setDoc } from "firebase/firestore";
+import { collection, doc, addDoc, getDoc, setDoc } from "firebase/firestore";
 import {ref, uploadBytes} from "@firebase/storage";
 import './evento-cadastro.css'
+import {Button, Form} from 'react-bootstrap';
 
 function EventoCadastro(){
 
@@ -20,7 +21,17 @@ const [fotoNova, setFotoNova] = useState();
 const [fotoAtual, setFotoAtual] = useState();
 const usuarioEmail = useSelector(state => state.usuarioEmail);
 const { id } = useParams();
+const [validated, setValidated] = useState(false);
 
+const handleSubmit = (event) => {
+  const form = event.currentTarget;
+  if (form.checkValidity() === false) {
+    event.preventDefault();
+    event.stopPropagation();
+  }
+
+  setValidated(true);
+};
 
 async function getDetails(){
     if(id){
@@ -39,25 +50,9 @@ async function getDetails(){
 
 useEffect(() => {
     getDetails();
-
-
-      // Fetch all the forms we want to apply custom Bootstrap validation styles to
-  const forms = document.querySelectorAll('.needs-validation')
-
-  // Loop over them and prevent submission
-  Array.from(forms).forEach(form => {
-    form.addEventListener('submit', event => {
-      if (!form.checkValidity()) {
-        event.preventDefault()
-        event.stopPropagation()
-      }
-
-      form.classList.add('was-validated')
-    }, false)
-  })
 }, []);
 
-function atualizar(){
+function update(){
     const docRef = doc(db, "eventos", id);
 
     setMsgTipo(null);
@@ -94,7 +89,7 @@ function atualizar(){
     });;
 }
 
-function cadastrar(){
+function register(){
 
     setMsgTipo(null);
     setCarregando(1);
@@ -140,52 +135,90 @@ function cadastrar(){
                 <h3 className="mx-auto font-weight-bold">{id ? "Atualizar Evento" : "Novo Evento"}</h3>
             </div>
 
-            <form className="needs-validation" novalidate>
-                <div className="form-group">
-                    <label>Titulo:</label>
-                    <input required type="text" className="form-control is-invalid" value={titulo && titulo} onChange={(e) => setTitulo(e.target.value)}/>
-                </div>
+            <Form noValidate validated={validated} onSubmit={handleSubmit}>
+                <Form.Group className="form-group">
+                    <Form.Label>Titulo:</Form.Label>
+                    <Form.Control
+                        required
+                        type="text"
+                        placeholder="Titulo"
+                        defaultValue={titulo && titulo}
+                        onChange={(e) => setTitulo(e.target.value)}
+                    />
+                    <Form.Control.Feedback type="invalid">
+                        Por favor escolha um título.
+                    </Form.Control.Feedback>
+                </Form.Group>
 
-                <div className="form-group">
-                    <label>Tipo do Evento</label>
-                    <select required className="form-control is-invalid" onChange={(e) => setTipo(e.target.value)} value={tipo && tipo}>
-                        <option>-- Selecione um tipo --</option>
+                <Form.Group className="form-group">
+                    <Form.Label>Tipo do Evento</Form.Label>
+                    <Form.Select aria-label="Default select example" required onChange={(e) => setTipo(e.target.value)} value={tipo && tipo}>
+                        <option value={''}>-- Selecione um tipo --</option>
                         <option>Festa</option>
                         <option>Teatro</option>
                         <option>Show</option>
                         <option>Evento</option>
-                    </select>
+                    </Form.Select>
+                    <Form.Control.Feedback type="invalid">
+                        Por favor escolha um tipo.
+                    </Form.Control.Feedback>
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                    <Form.Label>Descrição do Evento:</Form.Label>
+                    <Form.Control as="textarea" rows={3} onChange={(e) => setDetalhes(e.target.value)} value={detalhes && detalhes}/>
+                </Form.Group>
+                <div className="row">
+                <Form.Group className="col-6">
+                    <Form.Label>Data:</Form.Label>
+                        <Form.Control
+                            required
+                            type="date"
+                            placeholder="Data"
+                            defaultValue={data && data}
+                            onChange={(e) => setHora(e.target.value)}
+                        />
+                    <Form.Control.Feedback type="invalid">
+                        Por favor escolha uma data.
+                    </Form.Control.Feedback>
+                </Form.Group>
+
+                <Form.Group className="col-6">
+                    <Form.Label>Hora:</Form.Label>
+                        <Form.Control
+                            required
+                            type="time"
+                            placeholder="Hora"
+                            defaultValue={hora && hora}
+                            onChange={(e) => setHora(e.target.value)}
+                        />
+                    <Form.Control.Feedback type="invalid">
+                        Por favor escolha uma hora.
+                    </Form.Control.Feedback>
+                </Form.Group>
                 </div>
 
-                <div className="form-group">
-                    <label>Descrição do Evento:</label>
-                    <textarea onChange={(e) => setDetalhes(e.target.value)} value={detalhes && detalhes} className="form-control" rows="3"/>
-                </div>
-
-                <div className="form-group row">
-                    <div className="col-5">
-                        <label>Data:</label>
-                        <input required onChange={(e) => setData(e.target.value)} type="date" className="form-control is-invalid" value={data && data}/>
-                    </div>
-                    <div className="col-5">
-                        <label>Hora:</label>
-                        <input required onChange={(e) => setHora(e.target.value)} type="time" className="form-control is-invalid" value={hora && hora}/>
-                    </div>
-                </div>
-
-                <div className="form-group">
-                        <label>Upload da Foto:</label>
-                        <input required onChange={(e) => setFotoNova(e.target.files[0])} type="file" className="form-control is-invalid"/>
-                    </div>
+                <Form.Group className="mb-2">
+                    <Form.Label>Upload da imagem:</Form.Label>
+                        <Form.Control
+                            required
+                            type="file"
+                            placeholder="Imagem"
+                            onChange={(e) => setFotoNova(e.target.files[0])}
+                        />
+                    <Form.Control.Feedback type="invalid">
+                        Por favor escolha uma imagem.
+                    </Form.Control.Feedback>
+                </Form.Group>
 
                 <div className="row">
                     {
                     carregando > 0 ? <div className="spinner-border text-danger mx-auto" role="status"><span className="visually-hidden">Loading...</span></div>
-                     : <button type="submit"  onClick={id? atualizar : cadastrar} className="btn btn-lg btn-block mt-3 mb-5 btn-cadastro"> {id ? "Atualizar Evento" : "Publicar Evento"}</button>
+                     : <Button type="submit"  onClick={id? update : register} className="btn btn-lg btn-block mt-3 mb-5 btn-cadastro"> {id ? "Atualizar Evento" : "Publicar Evento"}</Button>
                     }
                 </div>
-            </form>
-            <div className="msg-login text-center my-5">
+            </Form>
+            <div className="msg-login text-center my-1">
                         {msgTipo === 'success' && <span style={{visibility: "visible"}}> Cadastro realizado! </span>}
                         {msgTipo === 'erro' && <span style={{visibility: "visible"}}> Algo deu errado... </span>}
                         {msgTipo === 'nullField' && <span style={{visibility: "visible"}}> Preencha todos os campos! </span>}
